48af590e1bf56677f7b46f2e58bb186d2eb3719f
JaTimur <JaTimur@s.sfusd.edu>
2025-01-30 11:56:45 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -6,7 +6,7 @@ public class PalindromeChecker {
 public void tester()
 {
   //String lines[] = loadStrings("palindromes.txt");
-  String[] lines = new String[6]; 
+  String[] lines = {"test", "rotator","rewriter","nurses run","Madam, I'm Adam!", "A Man! A Plan! A Canal! Panama!"};
     try{
         File myFile = new File("palindromes.txt");
         Scanner myReader = new Scanner(myFile);
@@ -23,27 +23,28 @@ public void tester()
         e.printStackTrace();
     }
   System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
-  {
-    if(palindrome(lines[i])==true)
-    {
+  for (int i=0; i < lines.length; i++){
+    if(isPalindrome(lowercaseNoSpace(lines[i]))==true){
       System.out.println(lines[i] + " IS a palindrome.");
     }
-    else
-    {
+    else{
       System.out.println(lines[i] + " is NOT a palindrome.");
     }
   }
 }
-public boolean palindrome(String word)
-{
-  //your code here
-  return false;
+public boolean isPalindrome(String word){
+  return(word.equals(reverse(word)));
 }
-public String reverse(String str)
-{
-    String sNew = new String();
-    //your code here
-    return sNew;
+public String reverse(String str){
+  String word="";
+  for(int n=str.length();n>0;n--){word=word+str.substring(n-1,n);}
+  return word;
+}
+public String lowercaseNoSpace(String str){
+  String word="";
+  for(int n=0;n<str.length();n++){
+    if(Character.isLetter(str.charAt(n))) word=word+str.charAt(n);
+  }
+  return word.toLowerCase();
 }
 }
