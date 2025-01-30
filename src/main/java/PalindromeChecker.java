314d48bef7bc4775496ae0b36d8c64b06fef4ef2
JaTimur <JaTimur@s.sfusd.edu>
2025-01-30 12:01:54 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -23,7 +23,7 @@ public void tester()
         e.printStackTrace();
     }
   System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++){
+  for (int i=0; i < lines.length(); i++){
     if(isPalindrome(lowercaseNoSpace(lines[i]))==true){
       System.out.println(lines[i] + " IS a palindrome.");
     }
